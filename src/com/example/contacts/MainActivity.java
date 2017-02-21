74902b2b479912a8a8e4d5d7ccf39d12b4cd11b9
Arun <aruntnj.av@gmail.com>
2017-02-21 16:35:36 +0530

    Second

--- src/com/example/contacts/MainActivity.java
+++ src/com/example/contacts/MainActivity.java
@@ -1,20 +1,44 @@
 package com.example.contacts;
 
+import android.content.Context;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 
-public class MainActivity extends ActionBarActivity {
 
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+
+	private EditText number;
+	private EditText name;
+	private Button save;
+	private Button view;
+	private SQLiteDatabase db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        createDatabase();
+        number = (EditText) findViewById(R.id.number);
+        name = (EditText) findViewById(R.id.name);
+        save = (Button) findViewById(R.id.save);
+        view = (Button) findViewById(R.id.view);
+        save.setOnClickListener(this);
+        view.setOnClickListener(this);
+     }
 
-    @Override
+    private void createDatabase() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
@@ -32,4 +56,10 @@ public class MainActivity extends ActionBarActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+	@Override
+	public void onClick(View arg0) {
+		// TODO Auto-generated method stub
+		
+	}
 }
